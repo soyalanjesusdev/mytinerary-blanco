a29e19151dd0725170970dcad39eea3d577bccbe
ajdata <blancodalan@gmail.com>
2021-11-17 21:37:29 -0300

    Nav

--- frontend/src/Components/SlideNav.js
+++ frontend/src/Components/SlideNav.js
@@ -1,48 +1,45 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Navbar,
-  Nav,
-  Container,
-  NavDropdown,
-} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+
 
 export default function Navegacion() {
   return (
-    <>
-      <Navbar bg="light"  expand="lg">
-        <Container fluid>
-          
-       
-          <Navbar.Brand href="#" >myTinerary</Navbar.Brand>
-          <img  src="." alt="logo" />
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="ms-auto my-2 my-lg-0 "
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-              
-            >
-                
-              <Nav.Link href="#action1" >Home</Nav.Link>
-              <Nav.Link href="#action2">Cities</Nav.Link>
-              <NavDropdown title="User" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Sing in</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Sing out
-                </NavDropdown.Item>
-               
-            
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Loggin
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="../Components/asset/logo.svg" alt="Logo" height="36"/>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li>
+              <hr class="dropdown-divider"/>
+            </li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </>
   );
 }
