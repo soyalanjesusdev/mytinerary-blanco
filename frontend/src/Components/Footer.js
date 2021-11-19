8cebab9c4381e9bed59a9afd142db172d7608548
ajdata <blancodalan@gmail.com>
2021-11-19 17:26:00 -0300

    footer

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,26 +1,38 @@
 const Footer = () => {
   return (
-    <footer >
-    <div>
-      <div class=" container-body">
-        <div class="colum2">
-          
-          <div class="d-flex redes">
-            <div class="iconos">
-              <div class="roww">                
-                <a href="https://www.facebook.com/francopet/" target="_blank"><img  src="./assets/facebook.png" /></a>
-                <a href="https://www.instagram.com/franco.pet-shop/" target="_blank"> <img src="./assets/instagram.png" /></a>              
-                <a href="https://www.twitter.com/francopet/" target="_blank"><img src="./assets/Twitter_Logo.png" /></a>
-                <a href="https://www.youtube.com/franco.pet-shop/" target="_blank"><img src="./assets/youtubeee.png" /></a>
-              </div>
-            </div>
-          </div>
-          </div>
-          </div>
-    </div>
-  </footer>
+    <footer class="page-footer font-small blue">
+      <div className="fotter">     <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Cities
+          </a>
+        </li>
+      </ul>
+      <div class="col-lg-3 col-md-5 mb-6">
+        <div class="view overlay z-depth-1-half">
+          <img
+            src="./styles/congreso-eeuu-1920-2.jpg"
+            class="img-fluid"
+            alt=""
+          />
+          <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a>
+        </div>
+      </div>
 
-
+      <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        <a href="#"> MyTinerary</a>
+      </div>
+      </div>
+ 
+    </footer>
   );
 };
 
